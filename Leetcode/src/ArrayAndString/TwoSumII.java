1e1169b978ef51eaf74df478231c0bf97d77a5a0
Ziwen Guan <ziweninus@gmail.com>
2016-04-03 23:21:08 -0400

    leetcode 1. Two Sum

---
+++ Leetcode/src/ArrayAndString/TwoSumII.java
@@ -0,0 +1,38 @@
+package ArrayAndString;
+
+import java.util.HashMap;
+/**
+ * 1. Two Sum
+ * @author zg55
+ *Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+
+You may assume that each input would have exactly one solution.
+
+Example:
+Given nums = [2, 7, 11, 15], target = 9,
+
+Because nums[0] + nums[1] = 2 + 7 = 9,
+return [0, 1].
+UPDATE (2016/2/13):
+The return format had been changed to zero-based indices. Please read the above updated description carefully.
+ */
+public class TwoSumII {
+    public int[] twoSum(int[] nums, int target) {
+        int result[] = new int[2];
+        HashMap<Integer, Integer> hm = new HashMap<Integer, Integer>();
+        for(int i=0; i< nums.length; i++) {
+            if(hm.get(target-nums[i])!=null) {
+                int j=hm.get(target-nums[i]);
+                return new int[]{j,i};
+            }
+            if(hm.get(nums[i])==null)
+                hm.put(nums[i], i);
+        }
+        return new int[]{-1,-1};
+    }
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
