43811d03954051211cb42ec0811ef8a9161e534a
Ziwen Guan <ziweninus@gmail.com>
2016-04-04 12:03:37 -0400

    leetcode 170. Two Sum III - Data structure design
    add linkedhashmap solution

--- Leetcode/src/ArrayAndString/TwoSumIII.java
+++ Leetcode/src/ArrayAndString/TwoSumIII.java
@@ -1,7 +1,8 @@
 package ArrayAndString;
 import java.util.ArrayList;
-import java.util.List;
 import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.List;
 /**
  * 170. Two Sum III - Data structure design
  * @author zg55
@@ -17,25 +18,39 @@ find(7) -> false
 Hide Company Tags LinkedIn
 Hide Tags Hash Table Design
 Hide Similar Problems (E) Two Sum (E) Unique Word Abbreviation
+https://leetcode.com/discuss/83367/explanation-java-and-code-pass-using-one-hashmap-around-90%25
  */
 public class TwoSumIII {
-	private List<Integer> list = new ArrayList<Integer>();
-	private HashMap<Integer, Integer> map = new HashMap<Integer,Integer>();
-    public void add(int number) {
-    	if(map.containsKey(number)) 
-    		map.put(number, map.get(number)+1);
-    	else {
-    		map.put(number,1);
-    		list.add(number);
-    	}
-    }
-    public boolean find(int value) {
-    	for(int i:list) {
-    		if((value-i==i&&map.get(i)>1)||(value-i!=i&&map.containsKey(value-i)))
-    			return true;
-    	}
-    	return false;
-    }
+//	private List<Integer> list = new ArrayList<Integer>();
+//	private HashMap<Integer, Integer> map = new HashMap<Integer,Integer>();
+	private LinkedHashMap<Integer, Integer> map = new LinkedHashMap<>();
+	public void add(int number) {
+	    map.put(number, map.getOrDefault(number,0)+1);
+	}
+
+	public boolean find(int value) {
+	    for (int n1:map.keySet()) {
+	        int n2 = value - n1;
+	        if (map.containsKey(n2) && (n1 != n2 || map.get(n1) > 1))
+	            return true;
+	    }
+	    return false;
+	}
+//    public void add(int number) {
+//    	if(map.containsKey(number)) 
+//    		map.put(number, map.get(number)+1);
+//    	else {
+//    		map.put(number,1);
+//    		list.add(number);
+//    	}
+//    }
+//    public boolean find(int value) {
+//    	for(int i:list) {
+//    		if((value-i==i&&map.get(i)>1)||(value-i!=i&&map.containsKey(value-i)))
+//    			return true;
+//    	}
+//    	return false;
+//    }
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
