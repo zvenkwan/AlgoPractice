5642d7951f7769a07cc411f9bd2eb31629b7bd5d
Ziwen Guan <ziweninus@gmail.com>
2016-04-06 20:07:40 -0400

    leetcode 219. Contains Duplicate II
    remove typos

---
+++ Leetcode/src/ArrayAndString/ContainsDuplicateII.java
@@ -0,0 +1,30 @@
+package ArrayAndString;
+
+import java.util.HashSet;
+
+/**
+ * 219. Contains Duplicate II
+ * @author zg55
+ *Given an array of integers and an integer k, find out whether there are two distinct indices i and j in the array such that nums[i] = nums[j] and the difference between i and j is at most k.
+
+Hide Company Tags Airbnb
+Hide Tags Array Hash Table
+Hide Similar Problems (E) Contains Duplicate (M) Contains Duplicate III
+
+ */
+public class ContainsDuplicateII {
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        HashSet<Integer> map = new HashSet<Integer>();
+        for(int i=0;i<nums.length;i++) {
+            if(i>k) map.remove(nums[i-k-1]);
+            if(!map.add(nums[i])) 
+                    return true;
+        }
+        return false;
+    }
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
