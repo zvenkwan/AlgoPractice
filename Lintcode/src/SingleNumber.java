35a5841817aae0ddc522fd2f67a1615328f8cc33
zvenkwan <ziweninus@gmail.com>
2018-05-02 18:00:47 -0400

    82. Single Number

--- Lintcode/src/SingleNumber.java
+++ Lintcode/src/SingleNumber.java
@@ -1,4 +1,32 @@
+/**
+ * 
+ * @author jguan
+ *82. Single Number 
+ Description
+ Notes
+ Testcase
+ Judge
+Discuss 
+Given 2*n + 1 numbers, every numbers occurs twice except one, find it.
 
+Have you met this question in a real interview? 
+Example
+Given [1,2,2,1,3,4,3], return 4
+
+Challenge 
+One-pass, constant extra space.
+ */
 public class SingleNumber {
-
+    /**
+     * @param A: An integer array
+     * @return: An integer
+     */
+    public int singleNumber(int[] nums) {
+        // Write your code here
+        int res = 0;
+        for(int i: nums) {
+            res ^= i;
+        }
+        return res;
+    }
 }
