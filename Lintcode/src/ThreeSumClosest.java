20aef1a1499cc3a235a1bdf3d1997187c9d0521c
zvenkwan <ziweninus@gmail.com>
2018-05-09 12:01:22 -0400

    59. 3Sum Closest

--- Lintcode/src/ThreeSumClosest.java
+++ Lintcode/src/ThreeSumClosest.java
@@ -1,5 +1,20 @@
 import java.util.Arrays;
+/**
+ * 
+ * @author jguan
+ *59. 3Sum Closest
+Description
+Given an array S of n integers, find three integers in S such that the sum is closest to a given number, target. Return the sum of the three integers.
 
+You may assume that each input would have exactly one solution.
+
+Have you met this question in a real interview?  
+Example
+For example, given array S = [-1 2 1 -4], and target = 1. The sum that is closest to the target is 2. (-1 + 2 + 1 = 2).
+
+Challenge
+O(n^2) time, O(1) extra space
+ */
 public class ThreeSumClosest {
     /**
      * @param numbers: Give an array numbers of n integer
