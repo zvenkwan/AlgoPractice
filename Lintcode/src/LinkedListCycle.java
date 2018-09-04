21c51b3dedb8a4dd7354fb8161291015367c34bf
zvenkwan <ziweninus@gmail.com>
2018-09-04 10:51:09 -0400

    102. Linked List Cycle

---
+++ Lintcode/src/LinkedListCycle.java
@@ -0,0 +1,34 @@
+/**
+ * 
+ * @author jguan
+ *102. Linked List Cycle
+Given a linked list, determine if it has a cycle in it.
+
+
+
+Example
+Given -21->10->4->5, tail connects to node index 1, return true
+
+Challenge
+Follow up:
+Can you solve it without using extra space?
+ */
+public class LinkedListCycle {
+    /**
+     * @param head: The first node of linked list.
+     * @return: True if it has a cycle, or false
+     */
+    public boolean hasCycle(ListNode head) {
+        // write your code here
+        if(head == null) return false;
+        ListNode slow = head;
+        ListNode fast = head.next;
+        if(fast == null) return false;
+        while(fast != null && slow != fast) {
+            slow = slow.next;
+            if(fast.next == null) return false;
+            fast = fast.next.next;
+        }
+        return fast != null;
+    }
+}
