acc48f714018162bc0013ddd051283590293f986
zvenkwan <ziweninus@gmail.com>
2018-10-29 14:09:40 -0400

    474. Lowest Common Ancestor II

--- Lintcode/src/LowestCommonAncestorofaBinaryTreeII.java
+++ Lintcode/src/LowestCommonAncestorofaBinaryTreeII.java
@@ -3,6 +3,7 @@ import java.util.LinkedList;
 /**
  * 
  * @author jguan
+ * 474. Lowest Common Ancestor II
  *Given the root and two nodes in a Binary Tree. Find the lowest common ancestor(LCA) of the two nodes.
 
 The lowest common ancestor is the node with largest depth which is the ancestor of both nodes.
