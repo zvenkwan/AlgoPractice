b14e30ade6eeeb085cc45633b7bb93668cd304af
zvenkwan <ziweninus@gmail.com>
2018-11-01 16:21:37 -0400

    380. Intersection of Two Linked Lists

---
+++ Lintcode/src/IntersectionofTwoLinkedLists.java
@@ -0,0 +1,56 @@
+/**
+ * 
+ * @author jguan
+ *
+380. Intersection of Two Linked Lists
+Write a program to find the node at which the intersection of two singly linked lists begins.
+
+Example
+The following two linked lists:
+
+A:          a1 → a2
+                   ↘
+                     c1 → c2 → c3
+                   ↗            
+B:     b1 → b2 → b3
+begin to intersect at node c1.
+
+Challenge
+Your code should preferably run in O(n) time and use only O(1) memory.
+
+Notice
+If the two linked lists have no intersection at all, return null.
+The linked lists must retain their original structure after the function returns.
+You may assume there are no cycles anywhere in the entire linked structure.
+ */
+public class IntersectionofTwoLinkedLists {
+    /**
+     * @param headA: the first list
+     * @param headB: the second list
+     * @return: a ListNode
+     */
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        // write your code here
+        ListNode pa = headA;
+        ListNode pb = headB;
+        
+        while(pa != pb) {
+            if(pa == null && pb != null) {
+                pa = headB;
+                pb = pb.next;
+                continue;
+            }
+            if(pb == null && pa != null) {
+                pb = headA;
+                pa = pa.next;
+                continue;
+            }
+            pa = pa.next;
+            pb = pb.next;
+        }
+        
+        return pa == null? null : pa;
+    }
+
+
+}
